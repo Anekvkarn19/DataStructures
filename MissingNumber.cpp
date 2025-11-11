bf0f0d2c923ca60eeb3d274ab528a2df99f643ce
Anekvarna <anek.varna030@gmail.com>
2025-11-11 20:15:05 +0530

    Added MissingNumber.cpp

---
+++ MissingNumber.cpp
@@ -0,0 +1,30 @@
+//missing number
+#include <iostream>
+#include <vector> 
+using namespace std;
+int main()
+{
+    int n,max=0;
+    cout<<"Enter the number of elements";
+    cin>>n;
+    vector<int>A(n);
+    cout<<"Enter the elements";
+    for(int i=0;i<n;i++)
+    {
+       cin>>A[i];
+       if(A[i]>max)
+        max=A[i];
+    }
+    
+    vector<int>DAT(max+1);
+    for(int k=0;k<n;k++)
+    {
+        DAT[A[k]]=1;
+    }
+    for(int k=1;k<=max;k++)
+    {
+        if(DAT[k]==0)
+         cout<<(k);
+    }
+    return 0;
+}
